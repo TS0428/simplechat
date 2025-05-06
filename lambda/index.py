9f38849246bc8710883bef2cf03548ed0a81adfc
TS0428 <tatsutosugioka@gmail.com>
2025-05-06 14:51:39 +0000

    fix index.py

--- lambda/index.py
+++ lambda/index.py
@@ -7,7 +7,7 @@ import re  # 正規表現モジュールをインポート
 import boto3
 
 # モデルID
-FASTAPI_URL = "https://eba9-34-16-166-116.ngrok-free.app"
+FASTAPI_URL = "https://e94c-34-82-5-125.ngrok-free.app"
 
 def lambda_handler(event, context):
     try:
@@ -18,23 +18,20 @@ def lambda_handler(event, context):
         payload = {
                 "prompt": message,
                 "max_new_tokens": 512,
-                "temprature": 0.7,
+                "temperature": 0.7,
                 "top_p": 0.9,
                 "do_sample": True
                 }
         data = json.dumps(payload).encode("utf-8")
         req = urllib.request.Request(
                 url = f"{FASTAPI_URL}/generate",
-                data = data
-                headers = {"Content-Type": "application/json"}
+                data = data,
+                headers = {"Content-Type": "application/json"},
                 method = "POST"
                 )
         with urllib.request.urlopen(req) as res:
             resp_text = res.read().decode()
             resp_json = json.loads(resp_text)
-        
-        # アシスタントの応答を取得
-        assistant_response = resp_json.get("generated_text", "")
         
         
         # 成功レスポンスの返却
@@ -47,8 +44,8 @@ def lambda_handler(event, context):
                 "Access-Control-Allow-Methods": "OPTIONS,POST"
             },
             "body": json.dumps({
-                "success": True,
-                "response": assistant_response
+                "generated_text": resp_json.get("generated_text", ""),
+                "response": resp_json.get("response_time", 0)
             })
         }
         
